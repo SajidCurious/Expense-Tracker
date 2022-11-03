89598b6a8768a5089c659f87c7d420464460a0c4
Mohammed <sajidcurious9@gmail.com>
2022-11-03 20:33:23 +0530

    commited

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const dateChange = (e) => {
+    setEnteredDate(e.target.value);
+  };
+  const titleChange = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+  const amountChange = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      date: enteredDate,
+      title: enteredTitle,
+      amount: enteredAmount,
+    };
+    console.log(expenseData);
+
+    setEnteredDate("");
+    setEnteredTitle("");
+    setEnteredAmount("");
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitForm}>
       <h1>Add an Expense</h1>
       <div className="input_date">
         <label>Date </label>
         <br />
-        <input type="date" className="label" />
+        <input type="date" value={enteredDate} onChange={dateChange} />
       </div>
       <div className="input_title">
         <label>Title</label>
         <br />
-        <input type="text" />
+        <input type="text" value={enteredTitle} onChange={titleChange} />
       </div>
-      <div className="input_amout">
+      <div className="input_amount">
         <label>Amount</label>
         <br />
-        <input type="number" />
+        <input type="number" value={enteredAmount} onChange={amountChange} />
       </div>
       <div>
         <button type="submit" className="create_expense">
